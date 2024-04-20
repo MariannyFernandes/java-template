8fb2c2eec09ae6282c528c2690c5c0c1fbf5f414
Marianny Fernandes <130843149+MariannyFernandes@users.noreply.github.com>
2024-04-20 19:06:44 +0000

    exercicioaaa

--- app/src/main/java/com/atv1/app/AppApplication.java
+++ app/src/main/java/com/atv1/app/AppApplication.java
@@ -1,13 +1,15 @@
 package com.atv1.app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class AppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		
+		int n = 0;
+
+		while (n < 10) {
+			System.out.println("oie, tudo bom?");
+			n = n + 1;
+		} System.out.println("Fim");
 	}
 
 }
